8d020f19dff0ed735fc353b3ee805838ec576ab9
Chris Roan <christophertroan@gmail.com>
2024-07-24 09:03:15 -0400

    pw match, initial login functionality

---
+++ login.js
@@ -0,0 +1,25 @@
+var objUsers = [
+    {
+        username: "Chris",
+        password: "pw123456"
+    },
+    {
+        username: "Tanner",
+        password: "pw654321"
+    }
+]
+
+function login() {
+    //retrieve data from user input (user and pw)
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    for(i = 0; i < objUsers.length; i++) {
+        if(username == objUsers[i].username && password == objUsers[i].password) {
+            console.log(username + " is logged in.")
+            return
+        }
+    } 
+}
+
+console.log("Incorrect username or password.");
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var myInput = document.getElementById("psw");
+var myInput2 = document.getElementById("psw2");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
+var pwMatch = document.getElementById("pw-match")
 
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+myInput2.onfocus = function() {
   document.getElementById("message").style.display = "block";
 }
 
@@ -13,8 +18,11 @@ myInput.onfocus = function() {
 myInput.onblur = function() {
   document.getElementById("message").style.display = "none";
 }
+myInput2.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
 
-// When the user starts to type something inside the password field
+// When the user starts to type something inside the first password field
 myInput.onkeyup = function() {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
@@ -54,4 +62,63 @@ myInput.onkeyup = function() {
     length.classList.remove("valid");
     length.classList.add("invalid");
   }
+  
+  //Validate pw match
+  if(myInput.value == myInput2.value) {
+    pwMatch.classList.remove("invalid");
+    pwMatch.classList.add("valid");
+  } else {
+    pwMatch.classList.remove("valid");
+    pwMatch.classList.add("invalid");
+  } 
+}
+// When the user starts to type something inside the second password field
+myInput2.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput2.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput2.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput2.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput2.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+  //Validate pw match
+  if(myInput.value == myInput2.value) {
+    pwMatch.classList.remove("invalid");
+    pwMatch.classList.add("valid");
+  } else {
+    pwMatch.classList.remove("valid");
+    pwMatch.classList.add("invalid");
+  } 
 }
